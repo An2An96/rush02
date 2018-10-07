fd5d0ebbe599f2f1a67a33dc9f875e12f00d1348
Rupert brax Schuppe <rschuppe@e2r15p1.21-school.ru>
2018-10-07 14:00:37 +0300

    update

--- ex00/includes/main.h
+++ ex00/includes/main.h
@@ -1,1 +1,2 @@
+void	ft_putstr(char *str);
 int		ft_get_rush_size(char *str, int *width, int *height);
--- ex00/srcs/main.c
+++ ex00/srcs/main.c
@@ -1,11 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rschuppe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/07 13:53:49 by rschuppe          #+#    #+#             */
+/*   Updated: 2018/10/07 13:53:51 by rschuppe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "main.h"
 #include <stdio.h>
 
-int main()
+void	ft_handler(char *str)
 {
-    int width;
-    int height;
-    ft_get_rush_size("o--o\n|  |\n|  |\no--o\n", &width, &height);
-    printf("width: %d, height: %d", width, height);
-    return (0);
+	int width;
+	int height;
+
+	if (!ft_get_rush_size(str, &width, &height))
+	{
+		ft_putstr("Size error");
+	}
+	else
+	{
+		printf("width: %d, height: %d", width, height);
+	}
+}
+
+int		main(void)
+{
+	ft_handler("o--o\n|  |\n|  |\no--o\n");
+	return (0);
 }
