7ebb2347fa0658e7a97b29e2991345991d9a8256
Rupert brax Schuppe <rschuppe@e2r14p6.21-school.ru>
2018-10-07 19:58:33 +0300

    fix norminette main.h and urils.h

--- ex00/includes/utils.h
+++ ex00/includes/utils.h
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rschuppe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/07 19:57:39 by rschuppe          #+#    #+#             */
-/*   Updated: 2018/10/07 19:57:40 by rschuppe         ###   ########.fr       */
+/*   Created: 2018/10/07 19:56:22 by rschuppe          #+#    #+#             */
+/*   Updated: 2018/10/07 19:56:24 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAIN_H
-# define MAIN_H
+#ifndef UTILS_H
+# define UTILS_H
 
-# include <unistd.h>
-# include <stdio.h>
-# include <fcntl.h>
-# include <stdlib.h>
-
-# include "utils.h"
-
-int	ft_get_rush_size(char *str, int *width, int *height);
-int	ft_get_rush_type(char *str, int cols, int rows, int types[3]);
-int	ft_validate_rush(char *str, int types[5], int width, int height);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+void	ft_putnbr(int nb);
 
 #endif
