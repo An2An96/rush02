a9a61173f093cde17035a516ce7882382627630c
Rupert brax Schuppe <rschuppe@e2r14p6.21-school.ru>
2018-10-07 21:24:30 +0300

    fix norminette ft_validate_rush

--- ex00/srcs/ft_validate_rush.c
+++ ex00/srcs/ft_validate_rush.c
@@ -11,46 +11,47 @@
 /* ************************************************************************** */
 
 #include "rush.h"
-#include <stdio.h>
 
-int ft_validate_rush(char *str, int types[5], int width, int height)
+int	ft_check_up_down_frame(char s, int types[5])
 {
-    int i;
-    int pos[4];
+	if ((types[0] && s != '-') || (types[1] && s != '*')
+		|| ((types[2] || types[3] || types[4]) && s != 'B'))
+		return (0);
+	return (1);
+}
 
-    //printf("ft_validate_rush: %s\n%d %d\n", str, width, height);
-    i = 0;
-    ft_compute_extreme(width, height, pos);
-    while (str[i] != '\0')
-    {
-        if (i != pos[0] && i != pos[1] && i != pos[2] && i != pos[3])
-        {
-            //printf("1. %d %d %d %d\n", pos[0], pos[1], pos[2], pos[3]);
-            //printf("2. %d %d %d %d %d\n", types[0], types[1], types[2], types[3], types[4]);
-            if ((i > pos[0] && i < pos[1]) || (i > pos[2] && i < pos[3]))
-            {
-                printf("Вверхняя/Нижняя стенка: %d - %c\n", i, str[i]);
-                if (types[0] && str[i] != '-')
-                    return (0);
-                if (types[1] && str[i] != '*')
-                    return (0);
-                if ((types[2] || types[3] || types[4]) && str[i] != 'B')
-                    return (0);
-            }
-            else if((width > 1 && height > 1) && (i % (width + 1) == 0 || ((i - (width - 1)) % (width + 1)) == 0))
-            {
-                printf("Левая/Правая стенка: %d - %c. %d %d\n", i, str[i], i % (width + 1), i % (i - (width - 1)));
-                if (types[0] && str[i] != '|')
-                    return (0);
-                if (types[1] && str[i] != '*')
-                    return (0);
-                if ((types[2] || types[3] || types[4]) && str[i] != 'B')
-                    return (0);
-            }
-            else if (str[i] != ' ' && str[i] != '\n')
-                return (0);
-        }
-        i++;
-    }
-    return (1);
+int	ft_check_left_right_frame(char s, int types[5])
+{
+	if ((types[0] && s != '|') || (types[1] && s != '*')
+		|| ((types[2] || types[3] || types[4]) && s != 'B'))
+		return (0);
+	return (1);
+}
+
+int	ft_validate_rush(char *str, int types[5], int width, int height)
+{
+	int i;
+	int pos[4];
+
+	i = -1;
+	ft_compute_extreme(width, height, pos);
+	while (++i >= 0 && str[i] != '\0')
+	{
+		if (i == pos[0] || i == pos[1] || i == pos[2] || i == pos[3])
+			continue;
+		if ((i > pos[0] && i < pos[1]) || (i > pos[2] && i < pos[3]))
+		{
+			if (ft_check_up_down_frame(str[i], types) == 0)
+				return (0);
+		}
+		else if ((width > 1 && height > 1)
+			&& (!(i % (width + 1)) || !((i - (width - 1)) % (width + 1))))
+		{
+			if (ft_check_left_right_frame(str[i], types) == 0)
+				return (0);
+		}
+		else if (str[i] != ' ' && str[i] != '\n')
+			return (0);
+	}
+	return (1);
 }
